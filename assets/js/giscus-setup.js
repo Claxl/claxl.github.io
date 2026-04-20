76a5a769ce4b6264b21a9599f5bc1ea5363d4339
Claxl <claxlxl@gmail.com>
2026-04-20 15:29:46 +0000

    Deploying to gh-pages from @ Claxl/claxl.github.io@8c63be332d66fce6de265673c14e250e57f68372 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Claxl/claudiodisalvo.github.io",
+    "data-repo": "Claxl/claxl.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
